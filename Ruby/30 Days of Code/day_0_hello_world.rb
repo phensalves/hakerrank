b297e61306f6ad72b7e7b61823070ee3c7212d1a
Pedro Alves <pedro_alves@me.com>
2016-04-18 17:27:53 -0300

    30 days of code day 0

---
+++ Ruby/30 Days of Code/day_0_hello_world.rb
@@ -0,0 +1,49 @@
+# Objective 
+# In this challenge, we review some basic concepts that will get you started with this series. Check out the Tutorial tab for learning materials and an instructional video!
+
+# Task 
+# To complete this challenge, you must save a line of input from stdin to a variable, print 𝙷𝚎𝚕𝚕𝚘, 𝚆𝚘𝚛𝚕𝚍.
+# Hello, World.
+#  on a single line, and finally print the value of your variable on a second line.
+
+# You've got this!
+
+# Note: The instructions are Java-based, but we support submissions in many popular languages. You can switch languages using the drop-down menu above your editor.
+
+# Input Format
+
+# A single line of text denoting inputString(the variable whose contents must be printed).
+# Welcome to 30 Days of Code!
+
+# Output Format
+
+# Print 𝙷𝚎𝚕𝚕𝚘, 𝚆𝚘𝚛𝚕𝚍. on the first line, and the contents of inputString on the second line.
+
+# Sample Input
+
+# Welcome to 30 Days of Code!
+# Sample Output
+
+# Hello, World. 
+# Welcome to 30 Days of Code!
+# Submissions
+
+# # Enter your code here. Read input from STDIN. Print output to STDOUT
+# inputString = gets # get a line of input from stdin and save it to our variable
+
+# # Your first line of output goes here
+# puts 'Hello, World.'
+
+# # Write the second line of output
+# $stdout.puts inputString
+
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+inputString = gets # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+puts 'Hello, World.'
+
+# Write the second line of output
+$stdout.puts inputString
