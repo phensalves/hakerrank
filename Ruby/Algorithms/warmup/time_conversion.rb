92975cb585b9f8342090142b133e64334f3c0d0c
Pedro Alves <pedro_alves@me.com>
2016-04-20 22:49:32 -0300

    time conversion workinh fine

--- Ruby/Algorithms/warmup/time_conversion.rb
+++ Ruby/Algorithms/warmup/time_conversion.rb
@@ -1,6 +1,6 @@
 time = gets.to_s
 hh = time.slice(0, 2).to_i
-mm = time.slice(time.index(":" + 1 , 2)).to_i
+mm = time.slice(time.index(":") + 1 , 2).to_i
 ss = time.slice(time.index(":", time.index(":") + 1) + 1, 2).to_i
 
 nh = hh
